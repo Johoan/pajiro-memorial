99fabe23ce5fc16f69300aec9f1f710a926cb785
Johoan <johoantoech@yahoo.com>
2022-12-17 21:18:40 +0100

    Initial project commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Menunav from './components/home/home';
+
+function App() {
+
+	
+
+	return (
+		<div>
+			<Menunav />
+		</div >
+	);
+}
+
+export default App;
+
+
