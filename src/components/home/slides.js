fed4bb4eb9b8a5bffc8ed364b91f15bf1a910fe7
Johoan <johoantoech@yahoo.com>
2022-12-20 17:03:08 +0100

    update with addittion of carousel and images

--- src/components/home/slides.js
+++ src/components/home/slides.js
@@ -1,42 +1,96 @@
-import React from "react";
-import { UncontrolledCarousel, Row, Col } from "reactstrap";
-import logo from '/home/john/Desktop/pajiro-memorial/pajiro-memorial/public/logo192.png';
-
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
 const items = [
   {
-    src:
-      {logo},
-    altText: "Slide 1",
-    caption: "",
-    header: "",
-    key: "1",
+    src: './images/school-photo.jpeg',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
   },
   {
-    src:
-      "https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/team-4.jpg",
-    altText: "Slide 2",
-    caption: "School library",
-    header: "",
-    key: "2",
+    src: './images/school-library.jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
   },
   {
-    src:
-      "https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/team-3.jpg",
-    altText: "Slide 3",
-    caption: "",
-    header: "",
-    key: "3",
-  },
+    src: './images/computer-lab.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
 ];
 
-const Slides = () => (
-  <Row>
-    <Col md="11.5" className="mx-auto">
-      <UncontrolledCarousel items={items} />
-    </Col>
-  </Row>
-);
+class Slides extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <img src={item.src} alt={item.altText} />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
+}
+
 
 export default Slides;
-
